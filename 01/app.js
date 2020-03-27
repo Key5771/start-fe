2a3369b19aea8bfc57bb2b6c2b020e7cb71bdc7f
김기현 <key@gimgihyeon-ui-MacBookPro.local>
2020-03-27 15:26:54 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,64 @@
+console.log('app.js');
+
+// var name = "김기현";
+// var Name = "김기현";
+
+// console.log(name);
+// console.log(Name);
+
+// var count = 1;
+// console.log(count);
+
+// // var isMan = true;   // bool
+// var isMan = "true"; // String
+// console.log(isMan, true)
+
+// // function log(str) {
+// //     console.log(str);
+// // }
+
+// var log = function(str) {
+//     console.log(str);
+// };
+
+// log("hello"); 
+
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+// };
+
+// // // var imageWidth = 100;
+// // // var imageHeight = 100;
+// // // var imageName = '1.png';
+
+// var image1 = [1, 2, 3, 4, 5, 6, 'a', {}];
+// image1[0] = 'a';
+
+// // console.log(image, image1);
+
+// var data = null;
+// var data1 = "";
+// var data2;
+
+// console.log(data, data1, data2);
+
+
+// console.log(1+2);
+// console.log('a' + 'b');
+// console.log(1 + '4');
+
+
+// console.log(1 == 1);
+// console.log(0 == "", 0 === "");
+// console.log('' == "0");
+
+// if (false) {
+//     console.log(1);
+// }
+
+
+console.log(typeof 1);
+console.log(typeof {});
