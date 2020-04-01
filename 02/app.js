d785e2d76cb23082594270563109755cb6030553
Key5771 <ksj57715@hanmail.net>
2020-04-01 19:00:59 +0900

    02 수업과제

---
+++ 02/app.js
@@ -0,0 +1,88 @@
+console.log("app");
+
+// Create
+var todos = ["운동"];
+var todo = "게임";
+todos.push(todo);
+console.log(todos);
+
+// Read
+todos.forEach(function(todo) {
+    console.log(todo);
+});
+
+
+// Update
+
+// 방법 1
+// var todos = ["운동", "게임"];
+// var updateTodo = "게임"
+// var updateIndex = todos.findIndex(function(todo) {
+//     return todo === updateTodo;
+// });
+// todos[updateIndex] = "공부";
+// console.log(todos);
+
+// 방법 2
+// var todos = ["운동", "게임"];
+// var updateTodo = "공부";
+
+// var newTodos = todos.map(function(todo) {
+//     if (todo === updateTodo) {
+//         return "공부";
+//     }
+//     return todo;
+// });
+
+// console.log(newTodos);
+
+
+// Delete
+// var todos = ["운동", "공부", "목욕"];
+// var deleteTodo = "공부";
+// var deleteIndex = todos.findIndex(function(todo) {
+//     return todo === deleteTodo;
+// });
+// todos.splice(deleteIndex, 1);
+// console.log(todos);
+
+// var isMan = true;
+// if (isMan) {
+//     console.log("남자");
+// } else {
+//     console.log("여자");
+// }
+
+// var gender = isMan ? "남자" : "여자"
+// console.log(gender);
+
+// window.alert("안녕하세요");
+// var who = prompt("당신은 누구세요?");
+// var isDelete = confirm("정말로 삭제하시겠습니까?");
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// console.log(sum(1, 2));
+
+// var car = {
+//     year: 2014,
+//     starting: function() {
+//         console.log("starting!!!");
+//     },
+// };
+
+// car.starting();
+
+// (function() {
+//     var a = "a";
+//     console.log(a);
+// })();
+
+function sum(x, y) {
+    if (!y) y = 1;
+    return x + y;
+}
+
+console.log(sum(3));
