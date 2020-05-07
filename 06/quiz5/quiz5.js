86081a400e85b2fd78f7064070bff326f4a1b3a3
Key5771 <ksj57715@hanmail.net>
2020-05-07 11:54:37 +0900

    airbna base 추가

--- 06/quiz5/quiz5.js
+++ 06/quiz5/quiz5.js
@@ -1,34 +1,31 @@
-const $box = document.querySelector(".box");
-const $box1 = document.querySelector(".box1");
+const $box = document.querySelector('.box');
+const $box1 = document.querySelector('.box1');
 
-let offset = {x : 0, y : 0};
+const offset = { x: 0, y: 0 };
 let isDown = false;
 
-$box.addEventListener("mousedown", mouseDown, false);
-$box1.addEventListener("mousedown", mouseDown, false);
-window.addEventListener("mouseup", mouseUp, false);
+$box.addEventListener('mousedown', mouseDown, false);
+$box1.addEventListener('mousedown', mouseDown, false);
+window.addEventListener('mouseup', mouseUp, false);
 
 function mouseDown(event) {
-    isDown = true;
-    offset.x = event.target.offsetLeft - event.clientX;
-    offset.y = event.target.offsetTop - event.clientY ;
+  isDown = true;
+  offset.x = event.target.offsetLeft - event.clientX;
+  offset.y = event.target.offsetTop - event.clientY;
 
-    window.addEventListener("mousemove", mouseMove)
+  window.addEventListener('mousemove', mouseMove);
 }
 
 function mouseUp() {
-    isDown = false;
-    window.removeEventListener("mousemove", mouseMove, true);
+  isDown = false;
+  window.removeEventListener('mousemove', mouseMove, true);
 }
 
 function mouseMove(event) {
-    if(!isDown) {
-        return;
-    }
+  if (!isDown) {
+    return;
+  }
 
-    event.target.style.left = event.clientX + offset.x + "px";
-    event.target.style.top = event.clientY + offset.y + "px";
+  event.target.style.left = `${event.clientX + offset.x}px`;
+  event.target.style.top = `${event.clientY + offset.y}px`;
 }
-
-
-
