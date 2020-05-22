937745b2dfba1dac1afbc509276b7bf092102c45
Key5771 <ksj57715@hanmail.net>
2020-05-22 16:38:28 +0900

    더보기 구현

--- 09/kakao.js
+++ 09/kakao.js
@@ -1,6 +1,7 @@
 const $btn = document.querySelector('#search-btn');
 const $text = document.querySelector('#search-text');
 const $result = document.querySelector('#result');
+const $moreBtn = document.querySelector('#more');
 const url = 'https://dapi.kakao.com/v2/search/web';
 const headers = { Authorization: 'KakaoAK cd6141155bb38de2c96510191479f0c1' };
 
@@ -10,7 +11,7 @@ function success(data) {
     return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
   });
 
-  $result.innerHTML = `<ul class="list-group list-group-flush">${li.join(
+  $result.innerHTML += `<ul class="list-group list-group-flush">${li.join(
     '',
   )}</ul>`;
 }
@@ -38,7 +39,14 @@ function search() {
 
   getResult();
 
+  $moreBtn.style.display = 'inline-block';
+
   return true;
 }
 
+function moreSearch() {
+  getResult();
+}
+
 $btn.addEventListener('click', search);
+$moreBtn.addEventListener('click', moreSearch);
