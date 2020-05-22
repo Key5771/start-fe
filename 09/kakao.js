01803bc8a0912a68a3232342af7f5c99f00424dd
Key5771 <ksj57715@hanmail.net>
2020-05-22 16:23:10 +0900

    오픈API사용

---
+++ 09/kakao.js
@@ -0,0 +1,44 @@
+const $btn = document.querySelector('#search-btn');
+const $text = document.querySelector('#search-text');
+const $result = document.querySelector('#result');
+const url = 'https://dapi.kakao.com/v2/search/web';
+const headers = { Authorization: 'KakaoAK cd6141155bb38de2c96510191479f0c1' };
+
+function success(data) {
+  const { documents } = data;
+  const li = documents.map((doc) => {
+    return `<li class="list-group-item"><a href="${doc.url}" target="_blank">${doc.contents}</a></li>`;
+  });
+
+  $result.innerHTML = `<ul class="list-group list-group-flush">${li.join(
+    '',
+  )}</ul>`;
+}
+
+function error() {
+  // eslint-disable-next-line no-alert
+  alert('데이터를 가져올 수 없습니다.');
+}
+
+function getResult() {
+  const { value } = $text;
+  fetch(`${url}?query=${value}`, { headers })
+    .then((res) => res.json())
+    .then(success)
+    .catch(error);
+}
+
+function search() {
+  const { value } = $text;
+  if (value === '') {
+    // eslint-disable-next-line no-alert
+    alert('검색어를 입력하세요.');
+    return false;
+  }
+
+  getResult();
+
+  return true;
+}
+
+$btn.addEventListener('click', search);
