3043d2a00da41cb6c8ebc325d1e96b698c63d7cb
key5771 <ksj57715@hanmail.net>
2020-06-04 17:34:46 +0900

    기본 html, package.json, eslint, prettier, webpack, style-loader, css-loader 설정

---
+++ 11/src/index.js
@@ -0,0 +1,7 @@
+import './todos.css';
+
+const $inputForm = document.querySelector('#input-form');
+const $input = document.querySelector('#input');
+const $result = document.querySelector('#result');
+
+console.log($inputForm);
