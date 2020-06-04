84d5a81a0e81bd53443f7b5a3119567742abdcc9
key5771 <ksj57715@hanmail.net>
2020-06-04 18:38:49 +0900

    delete, checkbox event 추가

--- 11/src/index.js
+++ 11/src/index.js
@@ -3,7 +3,27 @@ import formInput from './form-input';
 
 import './todos.css';
 
-const todos = [];
+const $result = document.querySelector('#result');
+
+const todos = [
+  {
+    text: 'aaa',
+    isDone: false,
+  },
+];
+
+$result.addEventListener('click', (event) => {
+  const { className } = event.target;
+  if (className === 'delete') {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === 'toggle-checked') {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
 
 formInput.init(todos);
 render(todos);
